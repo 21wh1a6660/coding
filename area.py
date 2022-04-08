8ba3e71b1a359efcbb246c3c47918ac4ca0ce5e0
21wh1a6660 <96616574+21wh1a6660@users.noreply.github.com>
2022-04-08 18:20:46 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,4 +1,8 @@
-pi = 22/7
-r = 1.1
-area = pi*r*r
+pi=22/7
+r=1.1
+area=pi*r*r
 print(area)
+#second code
+filename=input("enter the file name")
+file_extension="python"
+print("the extension of the file is",file_extension
